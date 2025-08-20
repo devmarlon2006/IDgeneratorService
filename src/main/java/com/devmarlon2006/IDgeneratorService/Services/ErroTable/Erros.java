8814e3985711b49babd31c834ab2271ba595dae0
Márlon <mo14421890@gmail.com>
2025-08-20 12:48:42 -0300

    Add Erros enum for error types
    Introduces the Erros enum to categorize different error types such as LIST_ERRO, ID_ERRO, NAME_ERRO, AGE_ERRO, COUNTRY_ERRO, and STATE_ERRO. This will help standardize error handling in the service.

---
+++ src/main/java/com/devmarlon2006/IDgeneratorService/Services/ErroTable/Erros.java
@@ -0,0 +1,12 @@
+package com.devmarlon2006.IDgeneratorService.Services.ErroTable;
+
+public enum Erros {
+
+    LIST_ERRO,
+    ID_ERRO,
+    NAME_ERRO,
+    AGE_ERRO,
+    COUNTRY_ERRO,
+    STATE_ERRO,
+
+}
