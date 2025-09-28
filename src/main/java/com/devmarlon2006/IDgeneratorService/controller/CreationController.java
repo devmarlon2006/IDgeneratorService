d3aff0859f57efc2f92a6660c355c7f48138350e
Márlon <mo14421890@gmail.com>
2025-09-28 17:21:31 -0300

    A API aceita qualquer tipo de classes

--- src/main/java/com/devmarlon2006/IDgeneratorService/controller/CreationController.java
+++ src/main/java/com/devmarlon2006/IDgeneratorService/controller/CreationController.java
@@ -2,30 +2,31 @@ package com.devmarlon2006.IDgeneratorService.controller;
 
 import com.devmarlon2006.IDgeneratorService.service.Config.GenerationPath;
 import com.devmarlon2006.IDgeneratorService.service.Intermediary.ControllerForService;
-import com.devmarlon2006.IDgeneratorService.service.model.POJO.Bytes;
 import com.devmarlon2006.IDgeneratorService.service.model.Response.CreationResponse;
 
 import jakarta.validation.Valid;
+import lombok.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Post")
 public class CreationController{
 
-    private final ControllerForService<Bytes> controllerForService;
+    private final ControllerForService<Object> controllerForService;
 
-    public CreationController(ControllerForService<Bytes> controllerForService) {
+    public CreationController(ControllerForService<Object> controllerForService) {
         this.controllerForService = controllerForService;
     }
 
+    @NonNull
     @PostMapping("/Create/id/{path}")
-    public CreationResponse<String> ControllerIDCreationClient(@RequestBody @Valid Bytes IDBytes, @PathVariable("path") GenerationPath path){
+    public CreationResponse<String> ControllerIDCreationClient(@RequestBody @Valid Object IDBytes, @PathVariable("path") GenerationPath path){
 
         final String GENERATED_ID;
 
         try{
 
-            GENERATED_ID = controllerForService.Generation(IDBytes, path);
+            GENERATED_ID = controllerForService.Generation( IDBytes, path);
 
         }catch (NullPointerException exception) {
             return new CreationResponse<>( null );
